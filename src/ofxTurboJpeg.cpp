3672a1d915b88edd976d3306c5da56b9fe029e67
Josh Silverman <josh@prettyextreme.com>
2012-04-09 18:44:10 -0400

    Adds loading image by reference and static buffers
    Now overloads load() so that we can load into an existing
    ofImage. By repeatedly loading into the same ofImage we
    will be able to avoid reallocating ofPixels and ofTextures
    for every load, drastically improving performance when
    playing back a jpen image sequence. (I was able to get 70fps for
    1280x720 jpegs compressed at 85% quality.) Note: Latest OF release
    will erroneously reallocate memory in this case. I am working to implement
    changes to ofImage. Changes should be implemented in the next
    few days in main of repo develop branch.
    
    Also, adds some static buffers to avoid reallocating potentially
    large buffers for reading/writing/decoding files. This also
    improves overall speed by ~10% in my test case.
    
    ALSO adds support for Windows. Yeah. That too.

--- src/ofxTurboJpeg.cpp
+++ src/ofxTurboJpeg.cpp
@@ -21,6 +21,11 @@ ofxTurboJpeg::ofxTurboJpeg(){
 	if (handleDecompress == NULL){
 		printf("Error in tjInitDeCompress():\n%s\n", tjGetErrorStr());
 	}
+	
+	fileBufferSize = 10000000;
+	imageBufferSize = fileBufferSize;
+	fileBuffer = (unsigned char*) malloc ( fileBufferSize ); 
+	imageBuffer = (unsigned char*) malloc ( imageBufferSize ); 
 }
 
 ofxTurboJpeg::~ofxTurboJpeg(){
@@ -30,8 +35,29 @@ ofxTurboJpeg::~ofxTurboJpeg(){
 	if (handleDecompress){
 		tjDestroy(handleDecompress);
 	}
+
+	free(fileBuffer);
+	free(imageBuffer);
 }
 
+void ofxTurboJpeg::setBufferSize(int bufferSize){	
+	
+	free(fileBuffer);
+	free(imageBuffer);
+
+	if(bufferSize > 0){
+		fileBufferSize = bufferSize;
+		fileBuffer = (unsigned char*) malloc ( fileBufferSize ); 
+		imageBufferSize = bufferSize;
+		imageBuffer = (unsigned char*) malloc ( imageBufferSize ); 
+
+	}else{
+		fileBufferSize = 1000; //Even when we're being stingy I'm sure we can spare 1K
+		fileBuffer = (unsigned char*) malloc ( fileBufferSize ); 
+		imageBufferSize = fileBufferSize;
+		imageBuffer = (unsigned char*) malloc ( imageBufferSize ); 
+	}
+}
 
 //rgb only for now...
 void ofxTurboJpeg::save( ofImage * img, string fileName, int jpegQuality ){
@@ -39,57 +65,95 @@ void ofxTurboJpeg::save( ofImage * img, string fileName, int jpegQuality ){
 	if (img == NULL) return;
 
 	int pitch = 0, flags = 0, jpegsubsamp = 0;
-	unsigned long size = 0;
 	int bpp = 3;	//rgb only for now...
 
-	unsigned char * output = (unsigned char*) malloc ( sizeof(char) * img->width * img->height * bpp ); 
+	unsigned char * output;
+	unsigned long size = sizeof(char) * img->width * img->height * bpp;
+	if(size <= fileBufferSize){
+		output = fileBuffer;
+	}else{
+		output = (unsigned char *)malloc(size);
+	}
+
 	tjCompress(handleCompress, img->getPixels() , img->width, pitch, img->height, bpp, output, &size, jpegsubsamp, jpegQuality, flags);
 
 	string filePath = ofToDataPath( fileName, false);
 	FILE * file = fopen( filePath.c_str(), "wb");
 	fwrite ( output , 1 , size , file );
 	fclose( file);
-	free (output);	
+	
+	if(output != fileBuffer){
+		free (output);	
+	}	
 }
 
 //rgb only for now...
 ofImage* ofxTurboJpeg::load(string fileName){
 	
-	ofImage * img = NULL;
-	
+	ofImage * img = new ofImage();
+
+	load(fileName,img);
+
+	return img;
+
+}
+
+bool ofxTurboJpeg::load(string fileName, ofImage* dstImg){
+
 	string filePath = ofToDataPath( fileName, false);
 	FILE * file = fopen( filePath.c_str(), "rb");
 
 	if ( file != NULL){
 
 		int flags = 0;
-		flags = TJ_FASTUPSAMPLE; //faster but worse!
+		flags = 0; //TJ_FASTUPSAMPLE would be faster but worse!
 	
-		img = new ofImage();
+		ofImage* img = dstImg;
 		struct stat info;
 		stat(filePath.c_str(), &info);	
-		unsigned char * pixels = (unsigned char *)malloc(info.st_size * sizeof(char));
+
+		unsigned char * pixels;
+		int size = info.st_size * sizeof(char);
+		if(size <= fileBufferSize){
+			pixels = fileBuffer;
+		}else{
+			pixels = (unsigned char *)malloc(size);
+		}
+
 		fread(pixels, info.st_size, 1, file);
 		fclose(file);
 		
 		int w; int h; int subsamp;
-		int ok = tjDecompressHeader2( handleDecompress, pixels, info.st_size, &w, &h, &subsamp );
+		int ok = tjDecompressHeader2( handleDecompress, pixels, size, &w, &h, &subsamp );
 		if (ok!=0) {printf("Error in tjDecompressHeader2():\n%s\n", tjGetErrorStr());}
 		
+		unsigned char* rgbData;
+		int useImageSize = 3 * w * h * sizeof(char);
+		if(useImageSize <= imageBufferSize){
+			rgbData = imageBuffer;
+		}else{
+			rgbData = (unsigned char *)malloc(useImageSize);
+		}
+
 		//printf("jpeg is %d x %d\n", w, h);
 		// 3 >> rgb only for now
-		unsigned char* rgbData = (unsigned char*) malloc(  3 * w * h * sizeof(char));	
-		tjDecompress( handleDecompress, pixels, info.st_size, rgbData, w, 0, h, 3 /* rgb only for now*/, 0 );
+		tjDecompress( handleDecompress, pixels, size, rgbData, w, w*3, h, 3 /* rgb only for now*/, 0 );
 		
 		img->setFromPixels( rgbData, w, h, OF_IMAGE_COLOR);
 		
-		delete rgbData;
-		delete pixels;
+		if(rgbData != imageBuffer){
+			delete rgbData;
+		}
+		if(pixels != fileBuffer){
+			delete pixels;
+		}
+
+		return true;
+
 	}else {
 		ofLog(OF_LOG_ERROR, "ofxTurboJpeg::load() >> Image %s not found", fileName.c_str());
+		return false;
 	}
-
 	
-	return img;
-
 }
+
